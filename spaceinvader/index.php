846bcbe2fd3e99ea481b0d53f1eff2dad0cc39f9
ijromz <101104824+ijromz@users.noreply.github.com>
2022-05-12 15:58:25 +0200

    Add files via upload
    update index

---
+++ spaceinvader/index.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+</head>
+<body>
+    <main class="landingBody">
+        <section class="landingSentence">  
+            <span style="--i:1">WHO</span>
+            <span style="--i:2">WILL</span>
+            <span style="--i:3">BE</span>
+            <span style="--i:4">THE</span>
+            <span style="--i:5">BEST?</span>    
+        </section>
+
+        <section class="landingSectionBtn">
+            <a href="login.php"><button class="landingBtn">Start</button></a>
+        </section>
+    </main>
+</body>
+</html> 
